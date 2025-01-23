4a2846740ad6ba635146c0620367c15a6b3f5f4a
Ahmad Dayeh <jnet230@gmail.com>
2025-01-23 15:56:49 +0200

    bug change

--- config/attendo.sql
+++ config/attendo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 22, 2025 at 12:11 AM
+-- Generation Time: Jan 23, 2025 at 02:56 PM
 -- Server version: 10.11.10-MariaDB
 -- PHP Version: 8.2.12
 
